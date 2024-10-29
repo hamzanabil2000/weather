8779f13446b4f48358d18ceba12e46e897269645
hamzanabil2000 <hamzapk@gmail.com>
2024-10-29 12:35:26 +0500

    Initial Commit

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -10,11 +10,13 @@ const Weather = ({ data }) => {
 
   const handleToggle = () => setIsCelsius(!isCelsius);
 
-  const currentDate = new Date().toLocaleDateString("en-GB", {
-    day: "2-digit",
-    month: "short",
-    year: "numeric",
-  }).replace(/ /g, "-");
+  const currentDate = new Date()
+    .toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    })
+    .replace(/ /g, "-");
 
   const currentTime = new Date().toLocaleTimeString([], {
     hour: "2-digit",
@@ -24,7 +26,6 @@ const Weather = ({ data }) => {
   return (
     <div>
       <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
-    
         <div className="flex justify-between items-center pb-4 text-lg">
           <span>{currentDate}</span>
           <span>{currentTime}</span>
@@ -40,21 +41,25 @@ const Weather = ({ data }) => {
             />
             <p className="text-2xl">{data.weather[0].main}</p>
           </div>
-            <p className="text-9xl">
-              {temperature}&#176;{isCelsius ? "C" : "F"}
-            </p>
+          <p className="text-9xl">
+            {temperature}&#176;{isCelsius ? "C" : "F"}
+          </p>
           <div className="flex items-center space-x-4">
             <button
               onClick={handleToggle}
               className="flex items-center justify-center text-lg bg-gray-800 text-white font-bold py-1 px-2 rounded-md border border-gray-500"
             >
-              <span className={`mr-1 ${!isCelsius ? "text-blue-400" : ""}`}>F</span>
+              <span className={`mr-1 ${!isCelsius ? "text-blue-400" : ""}`}>
+                F
+              </span>
               <span>|</span>
-              <span className={`ml-1 ${isCelsius ? "text-blue-400" : ""}`}>C</span>
+              <span className={`ml-1 ${isCelsius ? "text-blue-400" : ""}`}>
+                C
+              </span>
             </button>
           </div>
         </div>
-        
+
         <div className="bg-black/50 relative p-8 mt-4 rounded-md">
           <p className="text-2xl text-center pb-6">Weather in {data.name}</p>
           <div className="flex justify-between text-center">
